40637e7db8940dff3b49f7706585be1e98fcbe45
Tony Le <tle68@academic.rrc.ca>
2019-11-01 13:45:13 -0500

    Order model genrated

---
+++ SoccerShoesStore/db/migrate/20191101184501_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.integer :address_id
+      t.integer :user_id
+      t.Datetime :date
+      t.float :order_pst
+      t.float :order_gst
+      t.float :order_subtotal
+
+      t.timestamps
+    end
+  end
+end
