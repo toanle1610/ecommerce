b8358ae0578ac8c668867b933362325c0d44d19e
Tony Le <tle68@academic.rrc.ca>
2019-11-10 14:50:56 -0600

    Show detail product page

--- SoccerShoesStore/app/controllers/product_controller.rb
+++ SoccerShoesStore/app/controllers/product_controller.rb
@@ -3,9 +3,11 @@
 class ProductController < ApplicationController
   def index
     @products = Shoe.all
+    @categories = Shoe.distinct.pluck(:category)
   end
 
   def show
+    @categories = Shoe.distinct.pluck(:category)
     @product = Shoe.find(params[:id])
   end
 end
