2e96c3f8c686a1c3ccc97f860d2dbdd8b011d5e5
Tony Le <tle68@academic.rrc.ca>
2019-11-13 23:37:27 -0600

    Tried to hash password and save userinfo to db

--- SoccerShoesStore/app/controllers/user_controller.rb
+++ SoccerShoesStore/app/controllers/user_controller.rb
@@ -6,8 +6,16 @@ class UserController < ApplicationController
     @categories = Shoe.distinct.pluck(:category)
     # username = params[:]
   end
+
   def do_login
-    salt = SecureRandom.base64(8)
+    len   = ActiveSupport::MessageEncryptor.key_len
+    salt  = SecureRandom.hex len
+    key   = ActiveSupport::KeyGenerator.new(Rails.application.secrets.secret_key_base).generate_key salt, len
+    crypt = ActiveSupport::MessageEncryptor.new key
+    encrypted_data = crypt.encrypt_and_sign params[:password]
+    @password = "#{salt}$$#{encrypted_data}"
+    @username = params[:email]
+    User.create(username: @username, password: @password)
     redirect_to product_index_path
   end
 end
